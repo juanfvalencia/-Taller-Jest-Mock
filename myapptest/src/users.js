726a3c00a902b37336dfd2e43f8e58a1693a480a
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-12-13 00:58:53 -0500

    Se finaliza proyecto test con jest

---
+++ myapptest/src/users.js
@@ -0,0 +1,16 @@
+const axios = require('../node_modules/axios');
+class Users {
+  static all() {
+    return axios.get('/users.json').then(resp => resp.data);
+  }
+}
+module.exports = Users; const app = require('../src/App');
+const math = require('../src/math');
+
+test("calls math.add", () => {
+  const addMock = jest.spyOn(math, "add");
+  // calls the original implementation
+  expect(app.doAdd(1, 2)).toEqual(3);
+  // and the spy stores the calls to add
+  expect(addMock).toHaveBeenCalledWith(1, 2);
+});
